16524e23e0027e500eb1b13c63ea7d912cda4283
DomBarker99 <dominicbarker365@gmail.com>
2022-02-13 23:39:51 -0800

    Assignment 3.3 push

---
+++ Main33.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Main33 {
+  public static void main(String[] args) {
+    
+    Random rndm = new Random();
+    
+    int x = rndm.nextInt(100);
+    int y = rndm.nextInt(100);
+    int z = rndm.nextInt(100);
+
+    if ( x > y && y > z) {
+        System.out.println(x + " " + y + " " + z);
+    
+    } else if ( x > z && z > y) {
+        System.out.println(x + " " + z + " " + y);
+    
+    } else if ( y > z && z > x) {
+        System.out.println(y + " " + z + " " + x);
+    
+    } else if ( y > x && x > z) {
+        System.out.println(y + " " + x + " " + z);
+    
+    } else if ( z > x && x > y) {
+        System.out.println(z + " " + x + " " + y);
+    
+    } else if ( z > y && y > x) {
+        System.out.println(z + " " + y + " " + x);
+    }
+
+  }
+}
