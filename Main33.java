01d000af5c85d7543815e821f24a6ea4815bc8b3
DomBarker99 <dominicbarker365@gmail.com>
2022-02-13 23:44:55 -0800

    Fixed from descend to ascend

--- Main33.java
+++ Main33.java
@@ -9,22 +9,22 @@ public class Main33 {
     int y = rndm.nextInt(100);
     int z = rndm.nextInt(100);
 
-    if ( x > y && y > z) {
+    if ( x < y && y < z) {
         System.out.println(x + " " + y + " " + z);
     
-    } else if ( x > z && z > y) {
+    } else if ( x < z && z < y) {
         System.out.println(x + " " + z + " " + y);
     
-    } else if ( y > z && z > x) {
+    } else if ( y < z && z < x) {
         System.out.println(y + " " + z + " " + x);
     
-    } else if ( y > x && x > z) {
+    } else if ( y < x && x < z) {
         System.out.println(y + " " + x + " " + z);
     
-    } else if ( z > x && x > y) {
+    } else if ( z < x && x < y) {
         System.out.println(z + " " + x + " " + y);
     
-    } else if ( z > y && y > x) {
+    } else if ( z < y && y < x) {
         System.out.println(z + " " + y + " " + x);
     }
 
